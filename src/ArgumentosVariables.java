6358e48baf62587c6134581e0e78b6a5aef55d75
Elvis Almonte <elvisalmonte14@gmail.com>
2025-01-28 18:55:28 -0400

    Temas avanzados - Argumentos variables

--- src/ArgumentosVariables.java
+++ src/ArgumentosVariables.java
@@ -1,5 +1,17 @@
 public class ArgumentosVariables {
     public static void main(String[] args) {
-        System.out.println("Hola");
+      // imprimirNumeros(12,24);
+        variosParametros("Karla", 10, 20, 30);
+    }
+
+    private static void variosParametros(String nombre, int...numeros) {
+        System.out.println("Nombre: " + nombre);
+        imprimirNumeros(numeros);
+    }
+
+    private static void imprimirNumeros(int...numeros) {
+       for (int i = 0; i < numeros.length;i++){
+           System.out.print(numeros[i]);
+       }
     }
 }
